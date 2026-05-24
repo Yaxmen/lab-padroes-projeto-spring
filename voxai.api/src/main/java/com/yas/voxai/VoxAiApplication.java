d75fa81170cb1906137b4e6bf118fe21a58af911
Yasmim <129212589+Yaxmen@users.noreply.github.com>
2026-05-24 08:14:32 -0300

    Create VoxAiApplication.java

---
+++ voxai.api/src/main/java/com/yas/voxai/VoxAiApplication.java
@@ -0,0 +1,10 @@
+package com.yas.voxai;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoxAiApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(VoxAiApplication.class, args);
+  }
+}
